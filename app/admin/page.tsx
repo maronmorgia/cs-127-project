eced61ba1876a5f6063fee8ada0d0e2b36f2adb3
maronmorgia <mackimorgia@gmail.com>
2025-03-27 04:11:56 +0800

    feat: moved error & auth to route group

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,7 +7,7 @@ export default async function PrivatePage() {
 
   const { data, error } = await supabase.auth.getUser()
   if (error || !data?.user) {
-    redirect('/login')
+    redirect('/error')
   }
 
   const userRole = data.user.user_metadata?.role
