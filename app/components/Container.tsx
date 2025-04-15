4e2eb0be8b4c4fe8d750ec99f010d8fa61bf7caf
Lanzzhoyyy <lerecla@up.edu.ph>
2025-04-15 20:53:19 +0800

    feat: add responsive styles for laptop and hover effects on buttons

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -5,10 +5,14 @@ interface ContainerProps {
   children: ReactNode;
   className?: string;
 }
-  
+
 const Container: FC<ContainerProps> = ({ children, className = '' }) => {
   return (
-    <div className={`mx-auto max-w-[1920px] pt-[60px] pb-[60px] px-7 md:px-14 ${className}`}>{children}</div>
+    <div
+      className={`mx-auto max-w-[1920px] px-7 pt-15 pb-15 md:px-14 ${className}`}
+    >
+      {children}
+    </div>
   );
 };
 
--- app/components/FeaturesSection.tsx
+++ app/components/FeaturesSection.tsx
@@ -4,14 +4,14 @@ export default function FeaturesSection() {
   return (
     <section className='bg-primary-900'>
       <Container>
-        <header className=' h-[944px] flex flex-col justify-center items-center gap-14 md:h-[848px] '>
+        <header className='flex flex-col items-center justify-center gap-14'>
           <h1
             id='features-heading'
             className='self-stretch text-center leading-[48px]'
           >
             FEATURES
           </h1>
-          <div className='flex flex-col md:flex-row md:flex-wrap items-center justify-center gap-10 self-stretch'>
+          <div className='flex flex-col items-center justify-center gap-10 self-stretch md:flex-row md:flex-wrap'>
             {/* Feature: Quick */}
             <article className='inline-flex w-80 flex-col items-center justify-center gap-3'>
               <figure className='relative size-28 overflow-hidden'>
@@ -51,11 +51,6 @@ export default function FeaturesSection() {
                 Exclusive to UP email owners.
               </p>
             </article>
-          </div>
-          <div className='bg-secondary-900 inline-flex items-center justify-center gap-2.5 self-stretch rounded-md px-4 py-2'>
-            <p className='large leading-7 text-neutral-50'>
-              Log in with your UP email
-            </p>
           </div>
         </header>
       </Container>
